b30a4d0b907ef5cf7fa3b8a5dd75f767a85ca4c8
KumailHussain <kumi.leo1@gmail.com>
2017-10-11 12:11:56 -0700

    fixed v1

--- src/app/Dashboard/gallery/gallery.component.ts
+++ src/app/Dashboard/gallery/gallery.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css'],
 })
 
-export class  GalleryComponent {}
+export class  GalleryComponent implements OnInit{
+  config: any;
+
+  ngOnInit() {
+
+    this.config = [0,1,1,1,1,1,1]
+
+  }
+
+  changeMenu(item) {
+
+    for (var i in this.config) {
+      if(i ==item) {
+      this.config[i] = 0;
+      }
+      else {
+        this.config[i] = 1;
+      }
+    }
+    console.log(this.config)
+  }
+
+}
